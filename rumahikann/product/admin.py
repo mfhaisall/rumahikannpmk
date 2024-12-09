fd1b25415e6be02ba5362488581a60c3fc5fb8e6
mfhaisall <mhfaisal011@gmail.com>
2024-12-09 20:14:58 +0700

    commit 1

---
+++ rumahikann/product/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Ikan 
+# Register your models here.
+admin.site.register(Ikan)
---
+++ rumahikann/product/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Ikan(models.Model):
+	nama = models.CharField(max_length=50)
+	gambar = models.CharField(max_length=55)
+	harga = models.IntegerField()
+	type = models.CharField(max_length=55,default='emas')
+
+	def __str__(self):
+		return f'{self.nama} {self.harga} {self.gambar}'
+		
